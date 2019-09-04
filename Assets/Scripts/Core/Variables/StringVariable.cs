b29504e338e5cf11f4df525ca7ece78e8a8e9f25
Aistis Matulis <aistis@taktikal.is>
2019-09-04 23:04:52 +0200

    basic search form skeleton and form variables

---
+++ Assets/Scripts/Core/Variables/StringVariable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variables/String Variable")]
+public class StringVariable : ScriptableObject
+{
+    public string Description = "";
+
+    public string Value;
+
+    public void SetValue(string value)
+    {
+        Value = value;
+    }
+
+    public void SetValue(StringVariable value)
+    {
+        Value = value.Value;
+    }
+
+    public static implicit operator string(StringVariable reference)
+    {
+        return reference.Value;
+    }
+}
