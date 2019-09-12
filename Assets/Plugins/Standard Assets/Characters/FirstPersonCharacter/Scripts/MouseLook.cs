15ec4cd29a2eff529d11d9ded5c565c43ccde28c
Aistis Matulis <aistis@taktikal.is>
2019-09-12 13:54:29 +0200

    expandable pet ui with more info about the pet

--- Assets/Plugins/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Assets/Plugins/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -30,26 +30,28 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         public void LookRotation(Transform character, Transform camera)
         {
-            float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
-            float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
+            if (lockCursor) {
+                float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+                float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
 
-            m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
-            m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+                m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+                m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
 
-            if(clampVerticalRotation)
-                m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
+                if(clampVerticalRotation)
+                    m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
 
-            if(smooth)
-            {
-                character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
-                    smoothTime * Time.deltaTime);
-                camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
-                    smoothTime * Time.deltaTime);
-            }
-            else
-            {
-                character.localRotation = m_CharacterTargetRot;
-                camera.localRotation = m_CameraTargetRot;
+                if(smooth)
+                {
+                    character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
+                        smoothTime * Time.deltaTime);
+                    camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
+                        smoothTime * Time.deltaTime);
+                }
+                else
+                {
+                    character.localRotation = m_CharacterTargetRot;
+                    camera.localRotation = m_CameraTargetRot;
+                }
             }
 
             UpdateCursorLock();
@@ -74,25 +76,25 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void InternalLockUpdate()
         {
-            if(Input.GetKeyUp(KeyCode.Escape))
-            {
-                m_cursorIsLocked = false;
-            }
-            else if(Input.GetMouseButtonUp(0))
-            {
-                m_cursorIsLocked = true;
-            }
+//            if(Input.GetKeyUp(KeyCode.Escape))
+//            {
+//                m_cursorIsLocked = false;
+//            }
+//            else if(Input.GetMouseButtonUp(0))
+//            {
+//                m_cursorIsLocked = true;
+//            }
 
-            if (m_cursorIsLocked)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-            else if (!m_cursorIsLocked)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
+//            if (lockCursor)
+//            {
+//                Cursor.lockState = CursorLockMode.Locked;
+//                Cursor.visible = false;
+//            }
+//            else if (!lockCursor)
+//            {
+//                Cursor.lockState = CursorLockMode.None;
+//                Cursor.visible = true;
+//            }
         }
 
         Quaternion ClampRotationAroundXAxis(Quaternion q)
